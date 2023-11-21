ca7c2503227f61070c7bfc2ea9ceb352af010832
xaoccc <xaocccc@gmail.com>
2023-11-21 15:14:42 +0200

    setup

--- fruitipediaApp/urls.py
+++ fruitipediaApp/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-import fruitipediaApp.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
