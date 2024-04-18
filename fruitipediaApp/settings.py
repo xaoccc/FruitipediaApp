61afb0545e3e6b97a22f8eb43c42488dba368e4a
xaoccc <xaocccc@gmail.com>
2024-04-18 11:10:50 +0300

    requirements.txt

--- fruitipediaApp/settings.py
+++ fruitipediaApp/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "fruitipedia_db",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
+        "USER": "fruitipedia_postgres",
+        "PASSWORD": "6kWq6q0oB8tlH0tqLMsh2Zi0akK8eTs4",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
