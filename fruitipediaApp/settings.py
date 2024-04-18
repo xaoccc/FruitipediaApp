f759b25ab2909ec1d43e6a5575686646f500007f
xaoccc <xaocccc@gmail.com>
2024-04-18 11:37:39 +0300

    requirements.txt

--- fruitipediaApp/settings.py
+++ fruitipediaApp/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "NAME": "fruitipedia_db",
         "USER": "fruitipedia_postgres",
         "PASSWORD": "6kWq6q0oB8tlH0tqLMsh2Zi0akK8eTs4",
-        "HOST": "fruitipedia_db_bely",
+        "HOST": "dpg-cogd9msf7o1s73fs4jgg-a",
         "PORT": "5432",
     }
 }
