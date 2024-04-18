14328964f4bee2b164c2430240015b3665836101
xaoccc <xaocccc@gmail.com>
2024-04-18 11:53:17 +0300

    requirements.txt

--- fruitipediaApp/settings.py
+++ fruitipediaApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-cd%2g!p_@ut(kc8)%bd9_*@)i@kff^orkvy=!c#i!l+ak98)%0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fruitipediaapp.onrender.com']
 
 
 # Application definition
