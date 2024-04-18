10a6b1e8973c567528b3c6450f0a045f593150eb
xaoccc <xaocccc@gmail.com>
2024-04-18 12:09:13 +0300

    requirements.txt

--- fruitipediaApp/fruits/views.py
+++ fruitipediaApp/fruits/views.py
@@ -15,7 +15,8 @@ class ReadonlyViewMixin:
 class GetProfileMixin:
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["profile"] = Profile.objects.first()
+        if Profile.objects.first():
+            context["profile"] = Profile.objects.first()
         return context
 
 class HomePageView(GetProfileMixin, TemplateView):
@@ -28,7 +29,8 @@ class DashboardView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["profile"] = Profile.objects.first()
+        if Profile.objects.first():
+          context["profile"] = Profile.objects.first()
         return context
 
 class FruitCreateView(GetProfileMixin, CreateView):
