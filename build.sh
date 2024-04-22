2d7a401f86281c2e2847f4c5dd893787d0976ff8
xaoccc <xaocccc@gmail.com>
2024-04-22 12:53:56 +0300

    build.ch

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
+pip install gunicorn
 
 # Convert static asset files
 python manage.py collectstatic --no-input
