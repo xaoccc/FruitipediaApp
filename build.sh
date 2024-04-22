05dc0407c99e9c6dd89435cce0890863c1279112
xaoccc <xaocccc@gmail.com>
2024-04-22 13:01:26 +0300

    build.ch

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
-pip install gunicorn
 
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
