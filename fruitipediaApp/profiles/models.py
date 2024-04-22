0e27bf3508636f04052e46b554ab58b527200650
xaoccc <xaocccc@gmail.com>
2024-04-22 11:37:43 +0300

    dot env

--- fruitipediaApp/profiles/models.py
+++ fruitipediaApp/profiles/models.py
@@ -1,13 +1,13 @@
-from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.core.validators import MinLengthValidator
 from django.db import models
 from fruitipediaApp.profiles.validators import name_starts_with_letter
 
 
 class Profile(models.Model):
-    first_name = models.CharField(validators=(MaxLengthValidator(25), MinLengthValidator(2), name_starts_with_letter),)
-    last_name = models.CharField(validators=(MaxLengthValidator(35), MinLengthValidator(1), name_starts_with_letter),)
-    email = models.EmailField(unique=True, validators=(MaxLengthValidator(40),))
-    password = models.CharField(validators=(MaxLengthValidator(20), MinLengthValidator(8),),
+    first_name = models.CharField(max_length=25, validators=(MinLengthValidator(2), name_starts_with_letter),)
+    last_name = models.CharField(max_length=35, validators=(MinLengthValidator(1), name_starts_with_letter),)
+    email = models.EmailField(max_length=40, unique=True)
+    password = models.CharField(max_length=20, validators=(MinLengthValidator(8),),
                                 help_text='*Password length requirements: 8 to 20 characters')
     image = models.URLField(blank=True, null=True)
     age = models.IntegerField(blank=True, null=True, default=18)
