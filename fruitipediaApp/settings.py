d229427c86e284f0ab431e6a5c7dc15319f4e8b7
xaoccc <xaocccc@gmail.com>
2024-04-23 10:50:37 +0300

    req update

--- fruitipediaApp/settings.py
+++ fruitipediaApp/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fruitipediaapp.onrender.com']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
